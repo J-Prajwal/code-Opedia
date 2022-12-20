b0a769872fc4a22e47c11f204047b5ebc201026d
J-Prajwal <luckyjais058@gmail.com>
2022-12-20 18:33:49 +0530

    added: Dashboard page

---
+++ client/src/pages/Dashboard.tsx
@@ -0,0 +1,12 @@
+import { Heading } from '@chakra-ui/react'
+import React from 'react'
+
+const Dashboard = () => {
+  return (
+    <div>
+        <Heading>This is dashboard</Heading>
+    </div>
+  )
+}
+
+export default Dashboard
