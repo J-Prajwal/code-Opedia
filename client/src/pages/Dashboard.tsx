3ad99eba648eabc8a353882cde133903d718ea5e
GaganChandan2003 <101565932+GaganChandan2003@users.noreply.github.com>
2022-12-21 08:48:27 +0530

    Updated: Added responsiveness

--- client/src/pages/Dashboard.tsx
+++ client/src/pages/Dashboard.tsx
@@ -1,6 +1,4 @@
 import { Box, Grid, GridItem } from '@chakra-ui/react';
-import styles from "./Dashboard.module.css"
-import React from 'react'
 import Navbar from '../components/Navbar'
 import Footer from '../components/Footer';
 
@@ -8,17 +6,42 @@ const Dashboard = () => {
   return (
     <div>
       <Navbar />
-      <Box className={styles.outer_box}>
+      <Box
+        bg="#1a202c"
+        w="100%"
+        h={{ "md": "500vh", 'lg': "180vh" }}
+        borderTop="1px solid #9ca1cd"
+        padding={{ "base": "20px 20px", "sm": "20px 20px", "md": "30px 30px", "lg": "30px 100px" }}>
         <Grid
           height="100%"
-          templateRows='repeat(5, 1fr)'
+          templateRows='repeat(20, 2fr)'
           templateColumns='repeat(11, 1fr)'
           gap={4}>
-          <GridItem rowSpan={4} colSpan={3} bg='#a9aed6' borderRadius="5px" />
-          <GridItem colSpan={4} bg='#a9aed6' borderRadius="5px" />
-          <GridItem colSpan={4} bg='#a9aed6' borderRadius="5px" />
-          <GridItem colSpan={8} bg='#a9aed6' borderRadius="5px" />
-          <GridItem rowSpan={3} colSpan={8} bg='#a9aed6' borderRadius="5px" />
+          <GridItem
+            rowSpan={{ "base": 60, "sm": 60, "md": 40, "lg": 18 }}
+            colSpan={{ "base": 11, "sm": 11, "md": 11, "lg": 3 }}
+            bg='#a9aed6'
+            borderRadius="5px" />
+          <GridItem
+            rowSpan={{ "base": 13, "sm": 13, "md": 12, "lg": 4 }}
+            colSpan={{ "base": 11, "sm": 11, "md": 11, "lg": 4 }}
+            bg='#a9aed6'
+            borderRadius="5px" />
+          <GridItem
+            rowSpan={{ "base": 13, "sm": 13, "md": 12, "lg": 4 }}
+            colSpan={{ "base": 11, "sm": 11, "md": 11, "lg": 4 }}
+            bg='#a9aed6'
+            borderRadius="5px" />
+          <GridItem
+            rowSpan={{ "base": 13, "sm": 13, "md": 12, "lg": 4 }}
+            colSpan={{ "base": 11, "sm": 11, "md": 11, "lg": 8 }}
+            bg='#a9aed6'
+            borderRadius="5px" />
+          <GridItem
+            rowSpan={{ "base": 45, "sm": 45, "md": 40, "lg": 12 }}
+            colSpan={{ "base": 11, "sm": 11, "md": 11, "lg": 8 }}
+            bg='#a9aed6'
+            borderRadius="5px" />
         </Grid>
       </Box>
       <Footer />
