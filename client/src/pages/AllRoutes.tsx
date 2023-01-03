20e97f01fe301c93f3f03762f11f18454776fd70
J-Prajwal <luckyjais058@gmail.com>
2023-01-03 14:48:27 +0530

    update: /contests route added with Contests.tsx page

--- client/src/pages/AllRoutes.tsx
+++ client/src/pages/AllRoutes.tsx
@@ -3,13 +3,15 @@ import { Route, Routes } from "react-router-dom";
 import Home from "./Home";
 import Dashboard from "./Dashboard";
 import Signup from "./Signup";
+import Contests from "./Contests";
 
 const AllRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/:username" element={<Dashboard />} />
+      <Route path="/user/:username" element={<Dashboard />} />
       <Route path="/signup" element={<Signup />} />
+      <Route path="/contests" element={<Contests />} />
       {/* <Route path="/login" /> */}
     </Routes>
   );
