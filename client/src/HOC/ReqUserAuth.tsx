1a50708753d1a1ddc71723b1ccf657868280e9e2
J-Prajwal <luckyjais058@gmail.com>
2023-02-20 23:49:26 +0530

    Create private routes for admin & user

--- client/src/HOC/ReqUserAuth.tsx
+++ client/src/HOC/ReqUserAuth.tsx
@@ -4,12 +4,12 @@ import { State } from '../constants/constants';
 import { Navigate } from 'react-router-dom';
 
 const ReqAdminAuth = ({ children }: JsxElement) => {
-  const { userDetails } = useSelector((store: State) => store.auth);
+  const { isAuth } = useSelector((store: State) => store.auth);
 
-  if (userDetails?.is_admin) {
+  if (isAuth) {
     return children;
   } else {
-    return <Navigate to={'/401'} />;
+    return <Navigate to={"/"} replace />
   }
 };
 
