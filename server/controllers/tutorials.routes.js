b8f853f0492ef01dfd83ab090a10f661a0f1d7a9
J-Prajwal <luckyjais058@gmail.com>
2023-02-20 21:27:20 +0530

    update: tutorials route for update and delete is now ready

--- server/controllers/tutorials.routes.js
+++ server/controllers/tutorials.routes.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const TutorialModel = require("../models/tutorials.model");
 const isAdminAuthentication = require("../middlewares/isAdminAuthentication");
+const UserModel = require("../models/user.model");
 const tutorialController = express.Router();
 
 // getting all the tutes
@@ -38,5 +39,47 @@ tutorialController.post("/new", isAdminAuthentication, async (req, res) => {
     res.status(500).send({ message: "Internal server error" });
   }
 });
+
+tutorialController.patch(
+  "/update/:id",
+  isAdminAuthentication,
+  async (req, res) => {
+    const id = req.params.id;
+    const {
+      title,
+      description,
+      youtube_video_embed,
+      article_link,
+      category,
+      sub_category,
+    } = req.body;
+    try {
+      const data = await UserModel.findByIdAndUpdate(id, {
+        title,
+        description,
+        youtube_video_embed,
+        article_link,
+        category,
+        sub_category,
+      });
+      res.status(200).send({ message: "Tutorial updated!", data });
+    } catch (err) {
+      res.status(500).send({ message: "Internal server error" });
+    }
+  }
+);
+tutorialController.delete(
+  "/remove/:id",
+  isAdminAuthentication,
+  async (req, res) => {
+    const id = req.params.id;
+    try {
+      const data = await UserModel.findByIdAndDelete(id);
+      res.status(200).send({ message: "Tutorial deleted!", data });
+    } catch (err) {
+      res.status(500).send({ message: "Internal server error" });
+    }
+  }
+);
 
 module.exports = tutorialController;
