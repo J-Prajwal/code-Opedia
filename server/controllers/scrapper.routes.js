443a6bbfb0d26c50a5642b87e94dd84ea56c9fc9
J-Prajwal <luckyjais058@gmail.com>
2023-02-22 19:01:04 +0530

    added/server: scrapper to fetch the user data from hackerrank

--- server/controllers/scrapper.routes.js
+++ server/controllers/scrapper.routes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const axios = require('axios');
 const cheerio = require('cheerio');
 
-const Scrapper = express.router();
+const Scrapper = express.Router();
 
 Scrapper.get('hackerrank/:username', async (req, res) => {
   try {
